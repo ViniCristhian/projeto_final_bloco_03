4d70fdd664725422439143485890b790d5e62ec6
ViniCristhian <viniciuscristhian34@gmail.com>
2025-06-02 14:23:02 -0300

    feat: Implementação da rotas na aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './components/Footer/Footer'
 import Navbar from './components/Navbar/Navbar'
@@ -6,9 +7,18 @@ import Home from './pages/Home/Home'
 function App() {
   return (
     <>
-      <Navbar />
-      <Home />
-      <Footer />
+      <BrowserRouter>
+
+        <Navbar />
+
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/home' element={<Home />} />
+        </Routes>
+
+        <Footer />
+
+      </BrowserRouter>
     </>
   )
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,18 @@
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
         <header className="w-full flex justify-between items-center bg-sky-800 p-4 text-sky-100">
-            <h1 className="text-2xl font-bold">Farmácia da Villa</h1>
+            <Link to="/home" >
+                <h1 className="text-2xl font-bold">Farmácia da Villa</h1>
+            </Link>
             <nav>
                 <ul className="flex space-x-4">
                     <li>
-                        Categorias
+                        <Link to="/">Categorias</Link> 
                     </li>
                     <li>
-                        Sobre
+                        <Link to="/">Sobre</Link>
                     </li>
                 </ul>
             </nav>
